a55d7e26229305f07d5661faf6e8922aa711ab06
SangJin Jung <redcaprabbit1104@gmail.com>
2021-01-09 14:10:04 +0900

    ログイン画面のビューワイヤフレーム完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
+  devise_for :admins, controller: {
+    sessions: 'admins/sessions',
+    passwords: 'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'
+  }
+
+
   get 'boards/index'
   root to: "boards#index"
 end
